2f61d9d945dca77ed27cf7e31062a8d83ca5c8bc
Herczig <herczig.adam@gmail.com>
2019-09-18 17:48:36 +0200

    inheritence example and some comm protocol name added

--- hdr/test.hpp
+++ hdr/test.hpp
@@ -9,29 +9,66 @@
 
 using namespace std;
 
+enum comm_type
+{
+    SPI,
+    CAN,
+    UART,
+    I2C,
+    Bluetooh,
+    Wifi
+
+};
+
+enum dev_type
+{
+    Sensor,
+    Actuator,
+    Sensor_Actuator
+};
+
 class foo
 {
     public: 
     int i;
-    foo();
-    foo(int const i_);
+    foo() : i{10} {}
+    foo(int const i_) : i{i_} {}
     auto getFooInt(){ return i;}
 };
 
 class filehandler
 {
-    fstream fileHand;
-    string patternFile;
-    string lineFromFile;
-    const string newFile= "usable.txt";
-    vector<string> pattern;
+        fstream fileHand;
+        string patternFile;
+        string lineFromFile;
+        const string newFile= "usable.txt";
+        vector<string> pattern;
     public:
-    filehandler(const char &p): patternFile(&p) {};
-    void getPatternFileName();
-    void getInfoFromPattern();
-    void getLinesFromVector();
-    void processPattern();
-    void FinishProcess();
+        filehandler(const char &p): patternFile(&p) {};
+        void getPatternFileName();
+        void getInfoFromPattern();
+        void getLinesFromVector();
+        void processPattern();
+        void FinishProcess();
+};
+
+
+class device
+{
+    protected:
+        uint16_t id;
+        uint8_t commType;
+        string name;
+        uint8_t dev_Type;
+    public:
+        device(string Name, uint16_t ID, uint8_t devtype): id{ID}, name{Name}, dev_Type{devtype} {}
+};
+
+class sensor : protected device
+{
+    public:
+        sensor(string Name, uint16_t ID, uint8_t devtype) : device(Name,ID,devtype) {}
+
 };
 
 
