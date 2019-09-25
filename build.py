a989fd95071c29fc48a70a5a4a75e1d6c953ab3d
Herczig <herczig.adam@gmail.com>
2019-09-25 23:26:35 +0200

    added helper mode for build.py and some argumentum handling,

--- build.py
+++ build.py
@@ -9,22 +9,57 @@ cmd =  "cmake"
 ninja ="ninja"
 make = "make"
 isbuild = False
-idx = 0
-print  sys.argv[argNum-1]
+
 if argNum > 1:
+    helper = sys.argv[1]
+    if (helper == "--help")  or ( helper == "-h"):
+	print """
+	*********************Build.py********************
+
+	It is a simple python script to help easily build
+	and compile your project! You have to know it is
+	a hobby project and now it is at the begining.
+	Try to get ideas and implement to here.
+	
+	Usage: Add arguments for ./build.py. It can use
+	more than one arguments.
+	
+	Possibilities:
+	 -c or --cmake -> It will generate
+	cmake scripts and environment for build the project.
+	It will generate for ninja not for make.
+	
+	-b or --build -> If the CMakeLists.txt is proper 
+	and the script could generate the important cmake
+	files, then the python script try to compile the 
+	project with ninja.
+	
+	-x or --purge remove....
+	"""
+	exit()
+else:
+    print """
+    Use <python ./build --help> or
+    <python ./build -h> to get some
+    help about the building script 
+    """
+    exit()
+
+print  len(sys.argv)
+if argNum > 1:
+    temp = sys.argv[1]
     while cnt < argNum:
         print "cmaking and maybe compiling"
         
-        if sys.argv[cnt] == "-cn":
+        if temp  == "-c" or temp =="--cmake":
             cmd = cmd + " -GNinja ."
         
-        elif sys.argv[cnt] == "-c":
-            cmd = cmd+" ."
-        
-        elif sys.argv[cnt] == "-b":
+        elif  temp == "-b" or temp == "--build":
             isbuild = True
         
-        cnt = cnt +1
+	if argNum-1 > cnt:
+            cnt = cnt +1
+	    temp = sys.argv[cnt]
 
 os.system(cmd)
 
