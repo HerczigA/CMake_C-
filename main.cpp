31684794042351563d83eee70cf853f63c8b2107
adam <adam.herczig@ip-camp.com>
2019-10-15 14:43:13 +0200

    COMM : rebase failed, fixed errors

--- main.cpp
+++ main.cpp
@@ -9,10 +9,37 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+   
+    vector<unique_ptr<sensor>> Sensors;
+    vector<unique_ptr<actuator>> Actuators;
+    //vector<unique_ptr<actuator>> Actuators;
+    const char* JSONpath ="./JSON/pattern/template.json";
+    json_herczig::json JSONobj(*JSONpath);
+
+    if(!JSONobj.OpenPattern())
+        return E_JSON_OPEN;
+
+    JSONobj.bridgeGetSet(JSONobj.getSensorsNumber(),Sensors,Sensor);
+    JSONobj.bridgeGetSet(JSONobj.getActuatorsNumber(),Actuators,Actuator);
+    //JSONobj.bridgeGetSet(JSONobj.getSensorsNumber(),Se);
+
+#if DEBUG
+    cout << "\nSENSORS DETAILS " << endl; 
+    for(size_t i = 0; i < JSONobj.getSensorsNumber(); i++)
+    {
+        size_t j = 0;
+        cout<< "ID: " << Sensors[i]->get_ID() << endl;
+        cout<< "Name: " << Sensors[i]->get_Name() << endl;
+        cout<< "DevType: "  << (int) Sensors[i]->get_Dev_Type() << endl;
+        cout<< "CommType: " <<(int) Sensors[i]->get_Comm_Type() << endl;
+        for(auto it = 0; it < Sensors[i]->get_PinNumbers(); it++)
         {
             cout<< "Pins: " <<(int) Sensors[i]->get_Pins(j) << endl;
             j++;
         }
+            
+    }
+    cout << "\nACTUATORS DETAILS " << endl;
     for(size_t i = 0; i < JSONobj.getActuatorsNumber(); i++)
     {
         size_t j = 0;
@@ -25,35 +52,8 @@ int main(int argc, char *argv[])
             cout<< "Pins: " <<(int) Actuators[i]->get_Pins(j) << endl;
             j++;
         }
-   
-    vector<unique_ptr<sensor>> Sensors;
-    vector<unique_ptr<actuator>> Actuators;
-    const char* JSONpath ="./JSON/pattern/template.json";
-
-    if(!JSONobj.OpenPattern())
-        return E_JSON_OPEN;
-
-    JSONobj.bridgeGetSet(JSONobj.getSensorsNumber(),Sensors,Sensor);
-    JSONobj.bridgeGetSet(JSONobj.getActuatorsNumber(),Actuators,Actuator);
-    JSONobj.bridgeGetSet(JSONobj.getSensorsNumber(),Sensors);
-
-#if DEBUG
-    cout << "\nSENSORS DETAILS " << endl; 
-    for(size_t i = 0; i < JSONobj.getSensorsNumber(); i++)
-    {
-        cout<< "ID: " << Sensors[i]->get_ID() << endl;
-        cout<< "Name: " << Sensors[i]->get_Name() << endl;
-        cout<< "DevType: "  << (int) Sensors[i]->get_Dev_Type() << endl;
-        cout<< "CommType: " <<(int) Sensors[i]->get_Comm_Type() << endl;
     }
-    cout << "\nACTUATORS DETAILS " << endl;
-    for(size_t i = 0; i < JSONobj.getActuatorsNumber(); i++)
-    {
-        cout<< "ID: " << Actuators[i]->get_ID() << endl;
-        cout<< "Name: " << Actuators[i]->get_Name() << endl;
-        cout<< "DevType: "  << (int) Actuators[i]->get_Dev_Type() << endl;
-        cout<< "CommType: " <<(int) Actuators[i]->get_Comm_Type() << endl;
-    }
+    
 #endif
 
     
