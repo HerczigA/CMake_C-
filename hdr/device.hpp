0cb378fdd1b41ece40fb6b7a5aa10e60428595cf
adam <adam.herczig@ip-camp.com>
2019-12-27 14:18:28 +0100

    COMM: moved moveable part to parameters.hpp & started displayes

--- hdr/device.hpp
+++ hdr/device.hpp
@@ -6,71 +6,16 @@
 #include <string>
 #include <vector>
 #include "../Communication/communication.hpp"
-#include <wiringPi.h>
+#include "parameters.hpp"
+#include "../pwm/pwm.hpp"
+#ifdef RPI_VERSION
+    #include <wiringPi.h>
+#endif
 #include <unistd.h>
 #include <fcntl.h>
-
-#define DEBUG_DEVICE 0
-
-#define MAX_PORTS_NUMBER 21
-#define MAX_DC 1024
-#define PWM_RANGE_MAX 100
-
-#define MAX_SERVO_PORTS 8
-#define SERVO_STEP 200
-#define SERVO_HIGH_LIMIT 1050
-#define SERVO_LOW_LIMIT -150
-#define MIN_SERVO_DELAY_TIME 100
-#define DEF_SERVO_TIME 200
-#define DEF_SERVO_POSITION 500
-
-#define BUTTON_READ_TIME 50
-
-typedef uint8_t devType_t;
-typedef uint16_t Id_t;
-typedef uint8_t comm_t;
-typedef unsigned int time_ms_t;
+#include <memory>
 
 using namespace std;
-
-enum comm_type
-{
-    SPI = 1,
-    UART,
-    I2C,
-    PWM,
-    Bluetooth,
-    Unknow_communication,
-    COMMTYPE
-
-};
-typedef uint8_t pwm_t;
-
-enum pwm_type
-{
-    simplePWM = 0,
-    ServoPWM,
-    UnknowPWM,
-    PWMTYPE
-};
-
-enum pwm_err_t
-{
-    E_INIT_OK = 0,
-    E_INIT_NOK,
-    E_UNKNOW
-
-};
-
-enum dev_type
-{
-    SENSOR = 1,
-    ACTUATOR,
-    SENSOR_ACTUATOR,
-    Unknow_device,
-    DEVTYPE
-};
-
 
 class device
 {
@@ -79,13 +24,15 @@ class device
         comm_t commType;
         string name;
         devType_t dev_Type;
+        Communication_c com;
+        //pair pins-dirs??
         vector<uint8_t> pins;
         vector<uint8_t> dirs;
         uint8_t *directions;
 
         bool device_Initialized;
         bool communication_Initialized;
-        Communication_c com;
+        
 
     public:
         device();
@@ -106,19 +53,24 @@ class device
         void setcommType(comm_t commType);
         void setPinNumbers(uint8_t pins);
         void setPinsForuC(uint8_t pins, uint8_t dirs);
-        int InitDevice();
+        //int InitDevice();
 };
 
 class sensor : public device
 {
+    sens_type_t Sensor;
     bool buttonPushed;
     bool prevButtonState;
     bool buttonState;
     public:
         sensor(string Name, Id_t ID, devType_t devtype, comm_t commtype) : device(Name,ID,devtype,commtype) {
-            buttonPushed = false;
-            buttonState = LOW;
-            prevButtonState = LOW;
+            if(Sensor == Button)
+            {
+                buttonPushed = false;
+                buttonState = LOW;
+                prevButtonState = LOW;
+            }
+            
         }
         sensor() : device() {};
         void digital_Read(int pin);
@@ -128,26 +80,41 @@ class sensor : public device
 
 class actuator : public device
 {
-        int pwmRange ;
-        int initValue ;
+    std::unique_ptr<PWM> pwmObj;
 
       public:
-        actuator(string Name, Id_t ID, devType_t devtype, comm_t commtype) : device(Name,ID,devtype,commtype) {
-            pwmRange = PWM_RANGE_MAX;
-            initValue = 0;
+        actuator(string Name, Id_t ID, devType_t devtype, comm_t commtype, pwm_t *pwmtype) : device(Name,ID,devtype,commtype)
+        {
+            if(*pwmtype)
+                try
+                {
+                    pwmObj = std::make_unique<PWM>();
+                    throw "can not make pwmObj" ;
+                }    
+            
+
+            if(commType == PWM && pwm_Type == ServoPWM)
+            {
+                pwmObj->pwmRange = PWM_RANGE_MAX;
+                pwmObj->initValue = 0;
+            }
+            
         }
         actuator() : device() {};
-        pwm_t pwm_Setup(vector<uint8_t> pinNumber);
-        pwm_t pwm_ServoSetup(vector<uint8_t> pinNumbers,  uint8_t numberOfPorts);
-        pwm_t Init_PWM(int pwm, vector<uint8_t> pinNumbers, uint8_t numberOfPorts);
         void digital_Write(vector<int> pinNumbers,vector<int> states);
-        void pwm_Write(uint8_t pinNumber, int DC, time_ms_t  lengthOfDelay);
-        void pwm_Write_Breathing(uint8_t pinNumber, time_ms_t  lengthOfDelay);
-        void pwm_Servo_Write_In_Loop(uint8_t pinNumber, int16_t DC, time_ms_t lengthOfDelay, bool loop);
-        void pwm_Servo_Full_Limit(uint8_t pinNumber,time_ms_t t_length);
-        void pwm_Servo_Full_Limit(uint8_t pinNumber, time_ms_t t_length, bool button);
+       
 
 };
 
+class display : public device
+{
+    display_Type_t displayType;
+
+    public:
+        display() : device()  {};
+        display() : device()  {};
+    
+
+}
 
 #endif
--- src/device.cpp
+++ src/device.cpp
@@ -9,7 +9,6 @@
 #include <algorithm>
 #include <iterator>
 #include <bits/stdc++.h>
-#include <wiringPi.h>
 #include <softServo.h>
 #include <softPwm.h>
 using namespace std;
@@ -141,9 +140,12 @@ void device::setcommType(comm_t commType)
                 syslog(LOG_ERR, " I2C setup : %d ", result);
             break;
 
-       /* case PWM:
+        case PWM:
             Init_PWM();
-            break;*/
+            if(result)
+                syslog(LOG_ERR, " PWM setup : %d ", result);
+            device_Initialized = true;
+            break;
 
         case UART:
             result = com.Init_UART();
@@ -160,58 +162,7 @@ void device::setcommType(comm_t commType)
     }
 }
 
-pwm_t actuator::Init_PWM(int pwm, vector<uint8_t> pinNumbers, uint8_t numberOfPorts)
-{
 
-    pwm_t result = E_INIT_NOK;
-    switch (pwm)
-    {
-            case simplePWM:
-                result = pwm_Setup(pinNumbers);
-                break;
-
-            case ServoPWM:
-                result = pwm_ServoSetup(pinNumbers,numberOfPorts);
-                break;
-            default:
-                result = E_UNKNOW;
-                break;
-    }
-
-    return result;
-}
-/*
-void device::Init_Communication()
-{
-
-    switch(commType)
-    {
-        case SPI:
-            com.Init_SPI(com.SerialCom.spi);
-            break;
-
-        case I2C:
-            com.Init_I2C(com.SerialCom.i2c);
-            break;
-
-        case PWM:
-            Init_PWM();
-            break;
-
-        case UART:
-            com.Init_UART();
-            break;
-
-        case Bluetooth:
-            //Init_Bluetooth();
-            break;
-
-        default:
-            break;
-    }
-
-}
-*/
 void sensor::digital_Read(int pin)
 {
     if(pin >= MAX_PORTS_NUMBER)
@@ -342,170 +293,6 @@ uint8_t device::setPins(vector<uint8_t> pinNumbers)
     cout << "result = "<< result << endl;
 #endif
     return result;
-
-}
-pwm_t actuator::pwm_ServoSetup(vector<uint8_t> pinNumbers,  uint8_t numberOfPorts)
-{
-
-    int servoOut[] = {-1,-1,-1,-1,-1,-1,-1,-1};
-    pwm_t result = E_INIT_NOK;
-     if(pinNumbers.empty() || numberOfPorts >= MAX_SERVO_PORTS )
-         cout << "Nullpointer for pinNumbers or too much number for ports bastard!? " << endl;
-
-    else
-    {
-        int8_t i  = 0;
-        auto pinEnd = pinNumbers.end();
-        for(auto it = pinNumbers.begin(); it != pinEnd; ++it)
-            ++i;
-
-        if(i != numberOfPorts)
-        {
-
-            cout<< " not equal the given port numbers = "
-            <<(int) i << " and the gpio numbers = "
-            <<(int) numberOfPorts << endl;
-        }
-        else
-        {
-            for(auto j = 0; i >= 0; ++j)
-            {
-                servoOut[j] = pinNumbers[j];
-                i--;
-            }
-            result = E_INIT_OK;
-        }
-
-        softServoSetup(servoOut[0],servoOut[1],servoOut[2],servoOut[3],servoOut[4],servoOut[5],servoOut[6],servoOut[7]);
-        device_Initialized = true;
-
-    }
-    return result;
-}
-pwm_t actuator::pwm_Setup(vector<uint8_t> pinNumber)
-{
-    pwm_t result = E_INIT_NOK;
-    int pin = pinNumber[0];
-    if( !softPwmCreate(pin, initValue,  pwmRange))
-        {
-            device_Initialized = true;
-            result = E_INIT_OK;
-        }
-    return result;
-}
-
-void actuator::pwm_Write(uint8_t pinNumber, int DC, time_ms_t lengthOfDelay)
-{
-    if(!device_Initialized)
-        return;
-
-    pwmWrite(pinNumber,DC);
-    delay(lengthOfDelay);
-
-}
-
-void actuator::pwm_Write_Breathing(uint8_t pinNumber,time_ms_t lengthOfDelay)
-{
-    if(!device_Initialized)
-        return;
-
-    if(lengthOfDelay > 200)
-        lengthOfDelay = 200;
-
-    bool increase = true;
-    for(auto i =0 ; ; )
-    {
-        increase ? ++i : i--;
-
-        pwmWrite(pinNumber,i);
-        delay(lengthOfDelay);
-
-        if(i == MAX_DC )
-            increase = false;
-        else if(i == 0)
-            increase = true;
-
-    }
-}
-
-void actuator::pwm_Servo_Write_In_Loop(uint8_t pinNumber, int16_t DC,time_ms_t lengthOfDelay, bool loop)
-{
-    if(!device_Initialized)
-        return;
-    cout << "Start servo in Loop" << endl;
-    time_ms_t delaytime = lengthOfDelay;
-
-    if(delaytime <= MIN_SERVO_DELAY_TIME)
-        delaytime = DEF_SERVO_TIME;
-
-    if(loop)
-    {
-        while(loop)
-        {
-            softServoWrite(pinNumber,DC);
-            delay(delaytime);
-            DC -= SERVO_STEP;
-            if(DC < SERVO_LOW_LIMIT)
-                DC = SERVO_HIGH_LIMIT;
-        }
-    }
-    else
-    {
-        softServoWrite(pinNumber,DC);
-        delay(delaytime);
-    }
-
-
-}
-
-void actuator::pwm_Servo_Full_Limit(uint8_t pinNumber, time_ms_t t_length)
-{
-    if(!device_Initialized)
-        return;
-    cout << "Start servo FULL Limit" << endl;
-    time_ms_t delaytime = t_length;
-
-    if(delaytime <= MIN_SERVO_DELAY_TIME)
-        delaytime = DEF_SERVO_TIME;
-
-    softServoWrite(pinNumber,DEF_SERVO_POSITION);
-        delay(delaytime);
-
-    while(true)
-    {
-        softServoWrite(pinNumber,SERVO_HIGH_LIMIT);
-        delay(delaytime);
-        softServoWrite(pinNumber,SERVO_LOW_LIMIT);
-        delay(delaytime);
-    }
-
-}
-
-void actuator::pwm_Servo_Full_Limit(uint8_t pinNumber, time_ms_t t_length, bool button)
-{
-    if(!device_Initialized)
-        return;
-    time_ms_t delaytime = t_length;
-
-    if(delaytime <= MIN_SERVO_DELAY_TIME)
-        delaytime = DEF_SERVO_TIME;
-
-    static bool stateTop  = true;
-
-    if(button && stateTop)
-    {
-        cout<< "servo high" << endl;
-        softServoWrite(pinNumber,SERVO_HIGH_LIMIT);
-        delay(delaytime);
-        stateTop = false;
-    }
-    else if(!button && !stateTop)
-    {
-        cout<< "servo low" << endl;
-        softServoWrite(pinNumber,SERVO_LOW_LIMIT);
-        delay(delaytime);
-        stateTop = true;
-    }
 
 }
 
