df285c6bf915f39f4ed21c54084451a243fbe5dc
adam <adam.herczig@ip-camp.com>
2020-01-04 21:25:01 +0100

    COMP:some change dirhandling script

--- tools/pythonScripts/dirhandling.py
+++ tools/pythonScripts/dirhandling.py
@@ -6,4 +6,6 @@ import sys
 def MakeDir_And_Mv_binary(dirname, binary):
     if not os.path.exists(dirname):
         os.mkdir(dirname)
+    dirname = dirname+"/"
+    binary = binary + dirname
     os.system( binary )
