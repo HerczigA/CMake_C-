3d9dd964e6cd5b4be312caffe0890cf5708fcc41
HerczigDoc <adam.herczig@ip-camp.com>
2022-12-01 16:35:25 +0100

    cleanuped JSON part a little

--- JSON/json.cpp
+++ JSON/json.cpp
@@ -8,20 +8,13 @@ const char* const jsonPatternFile = "./JSON/pattern/template.json";
 
 JsonComponent::JsonComponent(const string &p)
     : mJsonFile(p)
-    , mDeviceNumber(0)
 {
-    
-    mDirections.push_back("IN");
-    mDirections.push_back("OUT");
-
 }
 
 JsonComponent::JsonComponent()
     : mJsonFile(jsonPatternFile)
-    , mDeviceNumber(0)
 {
-    mDirections.push_back("IN");
-    mDirections.push_back("OUT");
+
 }
 
 bool JsonComponent::openJson()
@@ -70,179 +63,43 @@ void JsonComponent::processing()
 {
     size_t found;
     json objects = mJsonHandler["Devices"];
-    set<string> devices = {"sensor", "actuator"};
-    for(auto& item : objects)
+    if(!objects.is_null())
     {
-        cout << item <<endl;
-        if(item.is_structured())
-            cout << item["Sensor"] <<endl;
+        auto items = objects.items();
+        for(auto& item : items)
+        {
+            Device tempdevice;
+            cout << item.key() <<endl;
+            cout << item.value() <<endl;
+
+            for(auto& iter : item.value().items())
+            {
+                if(iter.key() == "commtype")
+                {
+                    string pattern= iter.value();
+                    int value = getComType(pattern);
+                    cout << "found one"<< pattern  << value << endl;
+                }
+                cout << "iter.key()" << iter.key() <<endl;
+                cout << "iter.value()" << iter.value() <<endl;
+            }
+                
+            
+            // if(item.front() == "Sensor")
+            //     cout << "Sensor" <<endl;
+        }
     }
     
     
-    
-    
-    
-    
-    
-//     while(mPattern.begin() != mPattern.end())
-//     {
-
-//         auto temp = mPattern[0];
-//         auto tempEnd =temp.end();
-
-
-//          if( std::string::npos !=temp.find("Device" ))
-//         {
-//             mPattern.erase(mPattern.begin());
-//             temp = mPattern[0];
-//             bool checkDevicetype = true;
-
-//             for(; std::string::npos == temp.find("}" ); )
-//             {
-//                 if(checkDevicetype)
-//                 {
-//                     if(std::string::npos != temp.find("Sensor_Actuator"))
-//                     {
-//                         mSensorsAndActuators++;
-//                         mDevicetype.push_back(SENSOR_ACTUATOR);
-//                         checkDevicetype = false;
-//                     }
-
-//                     else if(std::string::npos != temp.find("Sensor"))
-//                     {
-//                         mSensors++;
-//                         mDevicetype.push_back(SENSOR);
-//                         checkDevicetype = false;
-//                     }
-
-//                     else if(std::string::npos != temp.find("Actuator"))
-//                     {
-//                         mActuators++;
-//                         mDevicetype.push_back(ACTUATOR);
-//                         checkDevicetype = false;
-//                     }
-
-//                     else if( tempEnd != std::find_if(temp.begin(),tempEnd,[] (char c)
-//                     {  return (c == '{') || (c =='}') ; }   ))
-//                         ;
-
-//                     else
-//                     {
-//                         mDevicetype.push_back(Unknown_device);
-//                     }
-//                 }
-//                 else
-//                 {
-//                     if( std::string::npos !=temp.find("commtype"))
-//                     {
-//                         size_t dev;
-//                         dev = getComType(temp);
-//                         mCommtype.push_back(dev);
-//                     }
-//                     else if( std::string::npos !=temp.find("name"))
-//                     {
-
-//                         found = temp.find_last_of(":");
-//                         temp = temp.substr(found+1);
-//                         temp.erase(0,2);
-//                         temp.erase(temp.end()-2,temp.end()-0);
-//                         mName.push_back(temp);
-//                     }
-//                     else if( std::string::npos !=temp.find("pin(s)"))
-//                     {
-//                         int pins = 1;
-//                         found = temp.find_last_of(":");
-//                         temp = temp.substr(found+1);
-//                         if( temp.end() != std::find_if(temp.begin(),temp.end(),[] (char c)
-//                             {  return (c == '[' ); }   ))
-//                         {
-//                             pins = calculatePinNumbers(temp);
-//                         }
-
-//                         else
-//                         {
-//                             temp.erase(temp.begin()+1);
-//                             mPinNumbers.push_back(atoi(temp.c_str()));
-//                         }
-
-//                         mPinOffset.push_back(pins);
-//                     }
-//                     else if( std::string::npos !=temp.find("dir"))
-//                     {
-//                         checkIO(temp);
-//                     }
-//                     else if( std::string::npos !=temp.find("ID"))
-//                     {
-//                         found = temp.find_last_of(":");
-//                         temp = temp.substr(found+1);
-//                         temp.erase(temp.begin()+1);
-//                         mId.push_back(atoi(temp.c_str()));
-//                         checkDevicetype = true;
-//                     }
-//                 }
-//                 mPattern.erase(mPattern.begin());
-//                 temp=mPattern[0];
-//             }
-
-
-//         }
-//         mPattern.erase(mPattern.begin());
-//     }
-
-//     mDeviceNumber = mActuators + mSensors + mSensorsAndActuators;
-
-// #if DEBUG
-//     std::cout << "Device number: " <<(int) deviceNumber
-//      << "\nActuator number:  " <<(int) mActuators
-//      << "\nSensor number: " <<(int) Sensors
-//      << "\nnSensor&Actuator number: " << (int)SensorsAndActuators<< std::endl;
-//      for(auto it = pinNumbers.begin(); it != pinNumbers.end(); ++it)
-//     {
-//         std::cout << "pinNumbers: " <<*it << std::endl;
-//     }
-// #endif
+ 
 }
 
 void JsonComponent::finishProcess()
 {
     
-    Make_Device(getSensorsNumber(), Vec_Sensors, SENSOR);
-    Make_Device(getActuatorsNumber(), Vec_Actuators, ACTUATOR);
-    Make_Device(getSensorsActuatorsNumber(), Vec_SensActuators, SENSOR_ACTUATOR);
-
-#if DEBUG_DEVICE
-    std::cout << "Details about devices after read" << std::endl;
-    size_t i = 0;
-    while(i < deviceNumber)
-    {
-
-        std::cout << std::endl;
-        std::cout << "ID: " << id[i] << std::endl;
-        std::cout << "Name: " << (std::string)name[i] << std::endl;
-        std::cout << "mDevicetype: " << mDevicetype[i] << std::endl;
-        std::cout << "CommType: " << commtype[i] << std::endl;
-        if(pinOffset[i])
-        {
-            std::cout << "pinOffset[i]: " << pinOffset[i] << std::endl;
-            for(size_t j = 0; j < pinOffset[i]; j++)
-            {
-                std::cout << "Pins: " << pinNumbers[i+j] << std::endl;
-            }
-        }
-        else
-            std::cout << "Pins: " << pinNumbers[i] << std::endl;
-
-        std::cout << std::endl;
-        i++;
-    }
-#endif
 }
 
 JsonComponent::~JsonComponent()
 {
-    mName.clear();
-    mDevicetype.clear();
-    mCommtype.clear();
-    mId.clear();
-
+    
 }
--- JSON/json.h
+++ JSON/json.h
@@ -15,17 +15,7 @@ class JsonComponent
         fstream mFileStream;
         json mJsonHandler; 
         string mJsonFile;
-        vector <string> mPattern;
-        vector <string> mName;
-        vector <string> mDirections;
-        vector <int> mDevicetype;
-        vector <int> mCommtype;
-        vector <int> mPinNumbers;
-        vector <int> mId;
-        vector <int> mPinOffset;
-        vector <int> mDirectionsLevel;
-        vector <int> mDirections_Offset;
-
+        
         map<string, uint8_t> CommType{
             {"spi", comm_type::SPI},
             {"uart", comm_type::UART},
@@ -33,19 +23,12 @@ class JsonComponent
             {"pwm", comm_type::PWM},
             {"bluetooth", comm_type::Bluetooth},
         };
-        uint8_t mDeviceNumber;
-        uint8_t mSensors = 0;
-        uint8_t mActuators = 0;
-        uint8_t mSensorsAndActuators = 0;
+        set<string> mDirections{"IN", "OUT"};
+        set<string> mDeviceKeyWords{"type", "comtype", "name", "pins", "direction(s)"};
+        set<string> mDatabaseKeyWords{"type", "host", "dbName", "user", "password"};
         uint8_t getComType(string &communication);
 
 
-        Id_t getID(int element) { return mId[element]; }
-        devType_t getDevType(int element) { return mDevicetype[element]; }
-        comm_t getComm(int element) { return mCommtype[element]; }
-        string &getName(int element) { return mName[element]; }
-        uint8_t getPins(int element) { return mPinNumbers[element]; }
-        uint8_t getDirs(int element) { return mDirectionsLevel[element]; }
         void processing();
         void finishProcess();
 
@@ -54,50 +37,7 @@ class JsonComponent
         JsonComponent(const string &p);
         JsonComponent();
         bool openJson();
-        const uint8_t getSensorsNumber() const { return mSensors; }
-        const uint8_t getActuatorsNumber() const { return mActuators; }
-        const uint8_t getSensorsActuatorsNumber() const { return mSensorsAndActuators; }
-
-        template<typename getdevicesNumber, typename VectorOfDevice, typename type>
-        void Make_Device(getdevicesNumber a, vector<unique_ptr<VectorOfDevice>>& b, type c)
-        {
-            
-            if(a)
-            {
-                int pin_Offset_Element = 0;
-                int direction_Offset_Element = 0;
-                const uint8_t all = mDeviceNumber;
-                for(uint8_t i = 0; i < all; i++)
-                {
-                    unique_ptr<VectorOfDevice> dev =make_unique<VectorOfDevice>();
-                    if(mDevicetype[i] == c)
-                    {
-                        dev->device::setID(getID(i));
-                        dev->device::setName(getName(i));
-                        dev->device::setdevType(getDevType(i));
-                        dev->device::setcommType(getComm(i));
-
-                        if(mDevicetype[i] == SENSOR_ACTUATOR)
-                        {
-                            for(uint8_t j = 0; j < mPinOffset[i]; j++)
-                            {
-                                dev->device::setPinsForuC(getPins(pin_Offset_Element+j),getDirs(direction_Offset_Element+j));
-                            }
-                            direction_Offset_Element += mDirections_Offset[i];
-                            dev->device::setPins(dev->device::get_Pins());
-                        }
-                        else
-                        {
-                            for(uint8_t j = 0; j < mPinOffset[i]; j++)
-                                dev->device::setPinNumbers(getPins(pin_Offset_Element+j));
-                        }
-
-                        b.push_back(move(dev));
-                    }
-
-                    pin_Offset_Element += mPinOffset[i];
-                }
-            }
-        }
+        
+        
         ~JsonComponent();
 };
